38939358221f0fce6174ca4e4602f430da2a3ab0
Dan Rogers <danielneilrogers@gmail.com>
2014-03-03 14:57:09 -0500

    Inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Millenniumsearch::Application.config.secret_key_base = '312db9cc43f47c46fe14b01e77059512b11f19cd47746bbe3a6128fb8066125154217700c4389721dc0545eeb7839c1c44ca3a8b830155e6edd8a30ce802d180'
